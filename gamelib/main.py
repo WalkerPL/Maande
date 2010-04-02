1759df815713591329ea5a55b0e628323b8a07ef
WalkerPL <marcin.pospiech@gmail.com>
2010-04-03 00:14:17 +0200

    improved responsiveness by changing event.poll() to event.get()

--- gamelib/main.py
+++ gamelib/main.py
@@ -81,61 +81,48 @@ def main():
 	p = Player()
 	items.append(Item())
 	ticker = pygame.time.Clock()
+	pygame.event.set_allowed(None)
+	pygame.event.set_allowed([pygame.QUIT, pygame.KEYDOWN, pygame.USEREVENT, pygame.MOUSEMOTION])
 
 	while True:
-		ticker.tick(40)
 		screen.fill(background_color)
-		event = pygame.event.poll() #check user input
-		if event.type == pygame.QUIT:
-			return False #end a game when clicking on x
 
 		if menu:
 		    drawtext("Do you want to enter the game?", 100, 100, 30, (255, 255, 255))
 		    drawtext("Press ENTER or ESCAPE", 100, 200, 40, (255, 255, 255))
-		    if event.type == pygame.KEYDOWN:
-		        if event.key == pygame.K_RETURN:
-		            menu = False
-		        elif event.key == pygame.K_ESCAPE:
-		            return False
-
-		elif not menu and event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-		    menu = True
 
 		else:
 		    
 		    if level_change == 1:
-		    	background_color = (randint(50, 255), randint(50, 255), randint(50, 255))
-		    	level += 1
-		    	pygame.time.set_timer(pygame.USEREVENT+1, 5000/level)
-		    	time_passed = 0
-		    	level_change = 0
+	        	background_color = (randint(50, 255), randint(50, 255), randint(50, 255))
+	        	level += 1
+	        	pygame.time.set_timer(pygame.USEREVENT+1, 5000/level)
+	        	time_passed = 0
+	        	level_change = 0
 		    elif level_change == 2:
-		    	pygame.time.wait(3000)
-		    	return False
-
-		    if event.type == pygame.USEREVENT+1:
-		        items.append(Item())
+	        	pygame.time.wait(3000)
+	        	return False
 
 		    for item in items:
 		        won = item.update(p)
 		        points += won
 		        if won > 0:
-		        	caught += 1
+		            caught += 1
 		        if won > 0 or item.y > 650:
 		            items.remove(item)
 
 		    mouse = pygame.mouse.get_pos()[0]
 		    p.update(mouse)
-		    
+		        
 		    time_passed += ticker.get_time()
 		    if time_passed >= 65000 and caught >= 9 * level:
-		    	level_change = 1
-		    	caught = 0
+		        level_change = 1
+		        caught = 0
 		    elif time_passed >= 65000 and caught < 9 * level:
-		    	level_change = 2
-		    	pygame.draw.rect(screen, (0,0,0), pygame.Rect(270, 270, 300, 60))
-		    	drawtext("GAME OVER", 283, 280, 64, (255, 0, 0))		    	
-		    
+		        level_change = 2
+		        pygame.draw.rect(screen, (0,0,0), pygame.Rect(270, 270, 300, 60))
+		        drawtext("GAME OVER", 283, 280, 64, (255, 0, 0))		    	
+		        
 		    # Draw the upper info
 		    pygame.draw.line(screen, (background_color[0] - 50, background_color[1] - 50, background_color[2] - 50), (0,29), (800,29), 58)
 		    pygame.draw.line(screen, (155, 155, 255), (265,0), (265, 58), 1)
@@ -148,6 +135,22 @@ def main():
 		    drawtext("You caught: %s" % caught, 280, 35, 22, (255, 255, 255))
 		    drawtext("To get through: %s" % (9 * level), 400, 35, 22, (255, 255, 255))
 		    drawtext("%s s" % ((65000 - time_passed)/1000), 720, 15, 48, (255, 255, 255))
-		    drawtext("fps: %.4s" %ticker.get_fps(), 735, 580, 18, (255, 255, 255))
+		    drawtext("fps: %.4s" % ticker.get_fps(), 735, 580, 18, (255, 255, 255))
+
+		for event in pygame.event.get():
+		    if event.type == pygame.QUIT:
+			    return False #end a game when clicking on x
+		    elif event.type == pygame.KEYDOWN:
+			    if event.key == pygame.K_RETURN:
+			        if menu:
+			            menu = False
+			    if event.key == pygame.K_ESCAPE:
+			        if menu:
+			            return False # close the game
+			        else:
+			            menu = True
+		    elif event.type == pygame.USEREVENT+1:
+			    items.append(Item())
 
 		pygame.display.flip()
+		ticker.tick(35)
